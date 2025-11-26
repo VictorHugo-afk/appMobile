ff6ecf41c7b97efbf8e0688ec889e924802d0959
Victor Hugo Ribeiro <ribeiro.santos.victor3007@escola.pr.gov.br>
2025-11-26 09:54:52 -0300

    dia 26/11

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,45 @@
-import { useState } from "react";
+function App(){
+  const[task, setTask] = useState([
+    {
+      id: 1,
+      title:"Estudar programação",
+      description:"Estudar programação paras se tornar um desenvolvedor full stack",
+      isCompleted:false
+    }
+    {
+      id:2,
+      title:"Estudar inglês",
+      description:"Estudar inglês para se tornar fluente",
+      isCompleted:false
+    }
+    {
+      id:3,
+      title:"Estudar matématica",
+      description:"Estudar matématica para desenvolver minh Lógica",
+      isCompleted:false
+    }
+  ]);
+
+  return(
+    <h1>Gerenciando tarefas</h1>
+  );
+}
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import { useState } from "react";
 function App(){
   const[message, setMessage] = useState('Olá, mundo');
   return(
